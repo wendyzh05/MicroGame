f0b16288e5722613959dd13cffd005b90359cca4
Wendy Zhang <wzhanghu@ucsc.edu>
2026-06-03 21:45:22 -0700

    Removed Spawnpoint and added sylveon sprite

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,7 +4,6 @@ public class FinishLine : MonoBehaviour
 {
     public Transform player1;
     public Transform player2;
-    public Transform spawnPoint;
     public Transform ai1;
     public Transform ai2;
     public AIRacer ai1Script;
@@ -20,39 +19,23 @@ public class FinishLine : MonoBehaviour
         {
             raceFinished = true;
             Debug.Log("Player 1 wins!");
-            Invoke(nameof(RestartRace), 2f);
         }
         else if (player2.position.x >= transform.position.x)
         {
             raceFinished = true;
             Debug.Log("Player 2 wins!");
-            Invoke(nameof(RestartRace), 2f);
         }
 
         else if (ai1.position.x >= transform.position.x)
         {
             raceFinished = true;
             Debug.Log("AI 1 wins!");
-            Invoke(nameof(RestartRace), 2f);
         }
         else if (ai2.position.x >= transform.position.x)
         {
             raceFinished = true;
             Debug.Log("AI 2 wins!");
-            Invoke(nameof(RestartRace), 2f);
         }
     }
 
-    void RestartRace()
-    {
-        player1.position = spawnPoint.position;
-        player2.position = spawnPoint.position + Vector3.down * 2f;
-        ai1.position = spawnPoint.position + Vector3.down * 1f;
-        ai2.position = spawnPoint.position + Vector3.down * 2f;
-
-        ai1Script.ResetSpeed();
-        ai2Script.ResetSpeed();
-
-        raceFinished = false;
-    }
 }
